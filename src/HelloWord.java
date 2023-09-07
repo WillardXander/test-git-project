7a56d1be492b58a244b2eb61d6916f33dd348d7a
wangxiang <13170251893@163.com>
2023-09-07 22:34:02 +0800

    Initial commit

---
+++ src/HelloWord.java
@@ -0,0 +1,12 @@
+/**
+ * @Author 15092
+ * @Date 2023/9/7
+ * @Version
+ * @Description
+ **/
+public class HelloWord {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+    }
+}
